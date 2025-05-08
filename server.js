677a8b98ce0eb2d1c0dc3b7e4766c964a5a52c64
umar-tech-spec <umar.afzal@dtcforce.com>
2025-05-08 05:49:23 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
